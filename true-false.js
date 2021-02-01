4b4180184393f0c4861ffd7c6b960267322f227a
RAIHAN <raihanbadsha363@gmail.com>
2021-02-01 23:03:29 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,22 @@
+// *******False⚔*******///
+/* 0
+   ""
+   undefined
+   null
+   NaN
+ */
+// *******Truthy⚔*******///
+/* 
+   "0"
+   " "
+   []
+   {}
+ */
+
+ let name = false;
+ console.log(name);
+ if (name) {
+     console.log("Condition is true");
+ } else {
+    console.log("Condition is false"); 
+ }
