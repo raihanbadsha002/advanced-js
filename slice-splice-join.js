ec5bc6745c80947658cd9c5a40a99d42ba9fb6bd
RAIHAN <raihanbadsha363@gmail.com>
2021-02-02 01:26:57 +0600

    slice

---
+++ slice-splice-join.js
@@ -0,0 +1,12 @@
+const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Slice array-----------------------------------❤❤❤
+// const sliceArray = numArray.slice(1,5);
+// console.log(sliceArray);
+
+// Splice Array----------------------------------❤❤❤
+const removed = numArray.splice(2,3);
+console.log(removed);
+
+// Join Array-----------------------------------❤❤❤
+// const joinArray = numArray.join("-");
+// console.log(joinArray);
